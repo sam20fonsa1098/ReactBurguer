a7b86eb240f4e59f5b9396a8fc955dace460b548
Samuel Cristo da Fonseca <samfonsa12345@gmail.com>
2020-04-12 10:13:22 -0400

    Modificando a estrutura do projeto

---
+++ src/container/App.js
@@ -0,0 +1,14 @@
+import React, {Component, useState, useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+  render(){
+    return(
+      <div
+      ></div>
+    );
+  };
+};
+
+export default App;
